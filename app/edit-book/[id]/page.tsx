7082a268e2b11206f6931f56a062dce5f29e7ced
Rifqin-11 <rifqinaufal9009@gmail.com>
2025-07-15 18:54:44 +0700

    add level 4

--- app/edit-book/[id]/page.tsx
+++ app/edit-book/[id]/page.tsx
@@ -211,6 +211,7 @@ export default function EditBookPage() {
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
+                        <option value="4">4</option>
                         <option value="A">A</option>
                         <option value="B1">B1</option>
                         <option value="B2">B2</option>
